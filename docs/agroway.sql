3935eec960b1bfcb2c2dfc4ff16ba4e84778ac95
Nsimba Afonso <nsimbaafonso0@gmail.com>
2025-06-10 07:48:25 +0100

    Banco de dados atualizado

--- docs/agroway.sql
+++ docs/agroway.sql
@@ -1,8 +1,10 @@
+CREATE DATABASE  IF NOT EXISTS `agroway` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `agroway`;
 -- MySQL dump 10.13  Distrib 8.0.42, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: agroway
 -- ------------------------------------------------------
--- Server version	5.5.5-10.4.32-MariaDB
+-- Server version	8.0.42
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -23,21 +25,22 @@ DROP TABLE IF EXISTS `avaliacao`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `avaliacao` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_avaliador` int(11) NOT NULL,
-  `id_avaliado` int(11) NOT NULL,
-  `id_pedido` int(11) NOT NULL,
-  `nota` int(11) DEFAULT NULL CHECK (`nota` >= 1 and `nota` <= 5),
-  `comentario` text DEFAULT NULL,
-  `data_avaliacao` timestamp NOT NULL DEFAULT current_timestamp(),
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_avaliador` int NOT NULL,
+  `id_avaliado` int NOT NULL,
+  `id_pedido` int NOT NULL,
+  `nota` int DEFAULT NULL,
+  `comentario` text,
+  `data_avaliacao` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `id_avaliador` (`id_avaliador`),
   KEY `id_avaliado` (`id_avaliado`),
   KEY `id_pedido` (`id_pedido`),
   CONSTRAINT `avaliacao_ibfk_1` FOREIGN KEY (`id_avaliador`) REFERENCES `usuario` (`id`),
   CONSTRAINT `avaliacao_ibfk_2` FOREIGN KEY (`id_avaliado`) REFERENCES `usuario` (`id`),
-  CONSTRAINT `avaliacao_ibfk_3` FOREIGN KEY (`id_pedido`) REFERENCES `pedido` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+  CONSTRAINT `avaliacao_ibfk_3` FOREIGN KEY (`id_pedido`) REFERENCES `pedido` (`id`),
+  CONSTRAINT `avaliacao_chk_1` CHECK (((`nota` >= 1) and (`nota` <= 5)))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -57,14 +60,14 @@ DROP TABLE IF EXISTS `categoria_produto`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `categoria_produto` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `nome` varchar(100) NOT NULL,
-  `descricao` text DEFAULT NULL,
-  `data_criacao` timestamp NOT NULL DEFAULT current_timestamp(),
-  `data_atualizacao` timestamp NOT NULL DEFAULT current_timestamp(),
+  `descricao` text,
+  `data_criacao` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `data_atualizacao` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `nome` (`nome`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -84,9 +87,9 @@ DROP TABLE IF EXISTS `entrega`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `entrega` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_pedido` int(11) NOT NULL,
-  `id_motorista` int(11) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_pedido` int NOT NULL,
+  `id_motorista` int NOT NULL,
   `status_entrega` enum('pendente','em trânsito','entregue') DEFAULT 'pendente',
   `data_inicio` datetime DEFAULT NULL,
   `data_fim` datetime DEFAULT NULL,
@@ -95,7 +98,7 @@ CREATE TABLE `entrega` (
   KEY `id_motorista` (`id_motorista`),
   CONSTRAINT `entrega_ibfk_1` FOREIGN KEY (`id_pedido`) REFERENCES `pedido` (`id`),
   CONSTRAINT `entrega_ibfk_2` FOREIGN KEY (`id_motorista`) REFERENCES `usuario` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -115,15 +118,15 @@ DROP TABLE IF EXISTS `estoque`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `estoque` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_produto` int(11) NOT NULL,
-  `quantidade` int(11) NOT NULL,
-  `data_criacao` timestamp NOT NULL DEFAULT current_timestamp(),
-  `data_atualizacao` timestamp NOT NULL DEFAULT current_timestamp(),
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_produto` int NOT NULL,
+  `quantidade` int NOT NULL,
+  `data_criacao` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `data_atualizacao` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `id_produto` (`id_produto`),
   CONSTRAINT `estoque_ibfk_1` FOREIGN KEY (`id_produto`) REFERENCES `produto` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -143,11 +146,11 @@ DROP TABLE IF EXISTS `funcao`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `funcao` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `nome` varchar(100) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `nome` (`nome`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -168,17 +171,17 @@ DROP TABLE IF EXISTS `itens_pedido`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `itens_pedido` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_pedido` int(11) NOT NULL,
-  `id_produto` int(11) NOT NULL,
-  `quantidade` int(11) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_pedido` int NOT NULL,
+  `id_produto` int NOT NULL,
+  `quantidade` int NOT NULL,
   `preco_unitario` decimal(10,2) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `id_pedido` (`id_pedido`),
   KEY `id_produto` (`id_produto`),
   CONSTRAINT `itens_pedido_ibfk_1` FOREIGN KEY (`id_pedido`) REFERENCES `pedido` (`id`),
   CONSTRAINT `itens_pedido_ibfk_2` FOREIGN KEY (`id_produto`) REFERENCES `produto` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -198,15 +201,15 @@ DROP TABLE IF EXISTS `notificacao`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `notificacao` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_usuario` int(11) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_usuario` int NOT NULL,
   `mensagem` text NOT NULL,
-  `lida` int(11) DEFAULT 1,
-  `data_envio` timestamp NOT NULL DEFAULT current_timestamp(),
+  `lida` int DEFAULT '1',
+  `data_envio` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `id_usuario` (`id_usuario`),
   CONSTRAINT `notificacao_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -226,16 +229,16 @@ DROP TABLE IF EXISTS `pagamento`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `pagamento` (
-  `id_pagamento` int(11) NOT NULL AUTO_INCREMENT,
-  `id_pedido` int(11) NOT NULL,
+  `id_pagamento` int NOT NULL AUTO_INCREMENT,
+  `id_pedido` int NOT NULL,
   `metodo_pagamento` enum('cartão','qrcode','paypal','visa') DEFAULT NULL,
   `valor_pago` decimal(10,2) NOT NULL,
   `status_pagamento` enum('pendente','confirmado','cancelado') DEFAULT 'pendente',
-  `data_pagamento` timestamp NOT NULL DEFAULT current_timestamp(),
+  `data_pagamento` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id_pagamento`),
   KEY `id_pedido` (`id_pedido`),
   CONSTRAINT `pagamento_ibfk_1` FOREIGN KEY (`id_pedido`) REFERENCES `pedido` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -255,17 +258,17 @@ DROP TABLE IF EXISTS `pedido`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `pedido` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_cliente` int(11) NOT NULL,
-  `data_pedido` timestamp NOT NULL DEFAULT current_timestamp(),
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_cliente` int NOT NULL,
+  `data_pedido` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `data_entrega` date DEFAULT NULL,
-  `local_entrega` text DEFAULT NULL,
+  `local_entrega` text,
   `status_pedido` enum('pendente','aceito','recusado','em trânsito','entregue') DEFAULT 'pendente',
-  `data_atualizacao` timestamp NOT NULL DEFAULT current_timestamp(),
+  `data_atualizacao` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `id_cliente` (`id_cliente`),
   CONSTRAINT `pedido_ibfk_1` FOREIGN KEY (`id_cliente`) REFERENCES `usuario` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -285,22 +288,22 @@ DROP TABLE IF EXISTS `produto`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `produto` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `nome` varchar(100) NOT NULL,
-  `descricao` text DEFAULT NULL,
+  `descricao` text,
   `preco` decimal(10,2) DEFAULT NULL,
-  `imagem_url` text DEFAULT NULL,
-  `id_categoria` int(11) NOT NULL,
-  `id_produtor` int(11) NOT NULL,
-  `data_criacao` timestamp NOT NULL DEFAULT current_timestamp(),
-  `data_atualizacao` timestamp NOT NULL DEFAULT current_timestamp(),
+  `imagem_url` text,
+  `id_categoria` int NOT NULL,
+  `id_produtor` int NOT NULL,
+  `data_criacao` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `data_atualizacao` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `nome` (`nome`),
   KEY `id_categoria` (`id_categoria`),
   KEY `id_produtor` (`id_produtor`),
   CONSTRAINT `produto_ibfk_1` FOREIGN KEY (`id_categoria`) REFERENCES `categoria_produto` (`id`),
   CONSTRAINT `produto_ibfk_2` FOREIGN KEY (`id_produtor`) REFERENCES `usuario` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -320,16 +323,16 @@ DROP TABLE IF EXISTS `recuperar_senha`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `recuperar_senha` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_usuario` int(11) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_usuario` int NOT NULL,
   `token` varchar(255) NOT NULL,
-  `data_solicitacao` datetime DEFAULT current_timestamp(),
+  `data_solicitacao` datetime DEFAULT CURRENT_TIMESTAMP,
   `data_expiracao` datetime DEFAULT NULL,
-  `usado` int(11) DEFAULT 0,
+  `usado` int DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `id_usuario` (`id_usuario`),
   CONSTRAINT `recuperar_senha_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -349,14 +352,14 @@ DROP TABLE IF EXISTS `rota`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `rota` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_motorista` int(11) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_motorista` int NOT NULL,
   `data_rota` date NOT NULL,
-  `descricao` text DEFAULT NULL,
+  `descricao` text,
   PRIMARY KEY (`id`),
   KEY `id_motorista` (`id_motorista`),
   CONSTRAINT `rota_ibfk_1` FOREIGN KEY (`id_motorista`) REFERENCES `usuario` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -376,15 +379,15 @@ DROP TABLE IF EXISTS `rota_pedidos`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `rota_pedidos` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_rota` int(11) NOT NULL,
-  `id_pedido` int(11) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_rota` int NOT NULL,
+  `id_pedido` int NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_rota` (`id_rota`,`id_pedido`),
   KEY `id_pedido` (`id_pedido`),
   CONSTRAINT `rota_pedidos_ibfk_1` FOREIGN KEY (`id_rota`) REFERENCES `rota` (`id`),
   CONSTRAINT `rota_pedidos_ibfk_2` FOREIGN KEY (`id_pedido`) REFERENCES `pedido` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -404,18 +407,18 @@ DROP TABLE IF EXISTS `usuario`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `usuario` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `nome` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
   `status` enum('ativo','inativo','pendente','banido') NOT NULL DEFAULT 'ativo',
-  `id_funcao` int(11) NOT NULL DEFAULT 1,
-  `data_criacao` timestamp NOT NULL DEFAULT current_timestamp(),
-  `data_atualizacao` timestamp NOT NULL DEFAULT current_timestamp(),
+  `id_funcao` int NOT NULL DEFAULT '1',
+  `data_criacao` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `data_atualizacao` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `id_funcao` (`id_funcao`),
   CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`id_funcao`) REFERENCES `funcao` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -424,7 +427,7 @@ CREATE TABLE `usuario` (
 
 LOCK TABLES `usuario` WRITE;
 /*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-INSERT INTO `usuario` VALUES (1,'Celson','celson@gmail.com','11111111','ativo',1,'2025-05-29 12:05:07','2025-05-29 12:05:07'),(2,'Márcio','marcion@gmail.com','22222222','ativo',2,'2025-05-29 12:05:07','2025-05-29 12:05:07'),(3,'Pedro','pedro@gmail.com','333','ativo',3,'2025-05-29 12:05:07','2025-05-29 12:05:07');
+INSERT INTO `usuario` VALUES (1,'Celson','celson@gmail.com','11111111','ativo',1,'2025-06-07 10:51:35','2025-06-07 10:51:35'),(2,'Márcio','marcion@gmail.com','22222222','ativo',2,'2025-06-07 10:51:35','2025-06-07 10:51:35'),(3,'Pedro','pedro@gmail.com','333','ativo',3,'2025-06-07 10:51:35','2025-06-07 10:51:35');
 /*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -437,4 +440,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-05-30 14:45:21
+-- Dump completed on 2025-06-07 11:54:45
