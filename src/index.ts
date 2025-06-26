06fc3bbc120336f6833d857234fd5a0e12ba404f
jenilson25francisco <franciscoviwaldo@gmail.com>
2025-06-26 01:14:56 +0100

    feat: acepting any front end

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ const app = express()
 app.use(express.json())
 const PORT = process.env.PORT || 3000
 
-const allowedOrigins = ['https://agroway-frontend.netlify.app']
+const allowedOrigins = ['*']
 const options: cors.CorsOptions = {
   origin: allowedOrigins,
   methods: 'GET,PUT,POST,DELETE',
