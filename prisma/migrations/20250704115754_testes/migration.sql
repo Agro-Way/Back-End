e58ef06fc58dbf944cab6c4f6521acbd13cc6aa4
Jenilson25Francisco <franciscoviwaldo@gmail.com>
2025-07-04 09:53:59 -0300

    feat: add migration scripts to create necessary tables and constraints

--- prisma/migrations/20250704115754_testes/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "OrderItem" DROP CONSTRAINT "OrderItem_productId_fkey";
-
--- DropForeignKey
-ALTER TABLE "cart_items" DROP CONSTRAINT "cart_items_productId_fkey";
-
--- DropForeignKey
-ALTER TABLE "stocks" DROP CONSTRAINT "stocks_productId_fkey";
